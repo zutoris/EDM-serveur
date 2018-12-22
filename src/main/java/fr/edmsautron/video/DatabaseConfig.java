ba5aad3ba2c0e53819a6a73340d83f441ac12595
zutoris <zutoris1975@yahoo.fr>
2018-12-22 12:11:37 +0100

    ajout bean pour résoudre l'erreur : HikariPool-1 - jdbcUrl is required with driverClassName.

--- src/main/java/fr/edmsautron/video/DatabaseConfig.java
+++ src/main/java/fr/edmsautron/video/DatabaseConfig.java
@@ -2,18 +2,31 @@ package fr.edmsautron.video;
 
 import javax.sql.DataSource;
 
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class DatabaseConfig {
-	@Bean
+	/*@Bean
 	@Primary
 	@ConfigurationProperties(prefix = "spring.datasource")
 	public DataSource dataSource() {
 		return DataSourceBuilder.create().build();
+	}*/
+	
+	@Bean
+	@Primary
+	@ConfigurationProperties(prefix = "spring.datasource")
+	public DataSourceProperties dataSourceProperties() {
+	    return new DataSourceProperties();
+	}
+
+	@Bean
+	@ConfigurationProperties(prefix = "spring.datasource")
+	public DataSource dataSource(DataSourceProperties properties) {
+	    return properties.initializeDataSourceBuilder().build();
 	}
 }
